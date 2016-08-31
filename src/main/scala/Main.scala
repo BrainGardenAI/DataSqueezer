37cb3dcac582e69f555d276d31409e634cf6f516
Silron88 <Silron88@gmail.com>
2016-08-31 12:17:18 +0300

    remove user seed

--- src/main/scala/Main.scala
+++ src/main/scala/Main.scala
@@ -4,9 +4,7 @@ import akka.event.Logging.InfoLevel
 import akka.http.scaladsl.Http
 import akka.http.scaladsl.server.Directives._
 import akka.stream.ActorMaterializer
-import com.google.inject.Guice
 import service.health._
-import service.users.{UserService, UserServiceModule}
 
 object Main extends App with HealthRoutes {
   
@@ -17,13 +15,7 @@ object Main extends App with HealthRoutes {
   val settings = Settings(system)
 
   val logger = Logging(system, getClass)
-
-  /** Use Guice for Dependency Injection. Remove if not required */
-  private val injector = Guice.createInjector(UserServiceModule)
-  private val userService = injector.getInstance(classOf[UserService])
   
-  val routes = logRequestResult("", InfoLevel)(userService.userRoutes ~ healthRoutes)
-
   Http().bindAndHandle(routes, settings.Http.interface, settings.Http.port) map { binding =>
     logger.info(s"Server started on port {}", binding.localAddress.getPort)
   } recoverWith { case _ => system.terminate() }
